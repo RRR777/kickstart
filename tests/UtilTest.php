e030ecac734c14b76f9643ae165eb7051d3942e2
RitaJ <ritaj777@gmail.com>
2018-12-03 19:30:54 +0200

    created clases and tests for them

---
+++ tests/UtilTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class UtilTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testMyIntentionalMistake()
+    {
+        $this->assertEquals(5, 2 + 2);
+    }
+}
